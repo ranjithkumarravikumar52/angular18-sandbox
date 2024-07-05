a93ef88c5898824e1ccb32a6017e452c96bf9631
ranjithkumarravikumar52 <ranjithkumarravikumar52@gmail.com>
2024-07-05 17:43:49 +0530

    able to render hardcoded days of the week

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.css'
 })
